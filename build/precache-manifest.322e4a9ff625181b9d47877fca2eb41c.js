7539b4c1488409dd729a785949fb4338e7877894
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-04-16 19:25:37 +0530

    Create build for the project

---
+++ build/precache-manifest.322e4a9ff625181b9d47877fca2eb41c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7de4ad3573b58a5932a4",
+    "url": "/static/js/main.cf5cc9da.chunk.js"
+  },
+  {
+    "revision": "6cf7e1e38fce15dd1096",
+    "url": "/static/js/2.7517a49d.chunk.js"
+  },
+  {
+    "revision": "7de4ad3573b58a5932a4",
+    "url": "/static/css/main.ed72dcb7.chunk.css"
+  },
+  {
+    "revision": "c5e48a3da831bd2b8959c34db794e8b3",
+    "url": "/index.html"
+  }
+];
